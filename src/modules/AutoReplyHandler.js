fb5730f5bc1700ae452d718165d403ade542aa91
QuangHuy/JustASweetPotato <kehuydiet09094@gmail.com>
2025-11-13 12:08:46 +0700

    Update AutoReplyHandler

--- src/modules/AutoReplyHandler.js
+++ src/modules/AutoReplyHandler.js
@@ -6,6 +6,7 @@ export default class AutoReplyHandler extends Handler {
     {
       match: "mmb",
       wildcard: false,
+      replyMention: true,
       content: [
         {
           replyMessage: true,
@@ -28,9 +29,17 @@ export default class AutoReplyHandler extends Handler {
 
     for (const item of this.autoReplyData) {
       if ((item.wildcard && message.content.includes(item.content)) || message.content.startsWith(item.match)) {
-        item.content.forEach(async (content) =>
-          content.replyMessage ? await message.reply({ content: content.content }) : await message.channel.send({ content: content.content })
-        );
+        item.content.forEach(async (content) => {
+          if (item.replyMention) {
+            const mentionUser = message.mentions.users.first();
+            content.replyMessage
+              ? await message.reply({ content: `<@${mentionUser.id}> ${content.content}` })
+              : await message.channel.send({ content: content.content });
+          } else {
+            content.replyMessage ? await message.reply({ content: content.content }) : await message.channel.send({ content: content.content });
+          }
+        });
+
         break;
       }
     }
