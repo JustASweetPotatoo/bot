8ff97b667fd70cf775410d87a58ea244593ec56f
QuangHuy/JustASweetPotato <kehuydiet09094@gmail.com>
2025-11-13 13:02:06 +0700

    Fix bug AutoReplyHandler

--- src/modules/AutoReplyHandler.js
+++ src/modules/AutoReplyHandler.js
@@ -6,7 +6,7 @@ export default class AutoReplyHandler extends Handler {
     {
       match: "mmb",
       wildcard: true,
-      replyMention: true,
+      mentionUser: true,
       content: [
         {
           replyMessage: false,
@@ -27,21 +27,23 @@ export default class AutoReplyHandler extends Handler {
   onMessage = async (message) => {
     if (message.content.startsWith("c>")) return;
 
-    for (const item of this.autoReplyData) {
-      if ((item.wildcard && message.content.includes(item.content)) || message.content.startsWith(item.match)) {
-        item.content.forEach(async (content) => {
-          if (item.replyMention) {
-            const mentionUser = message.mentions.users.first();
-            content.replyMessage
-              ? await message.reply({ content: `<@${mentionUser.id}> ${content.content}` })
-              : await message.channel.send({ content: content.content });
-          } else {
-            content.replyMessage ? await message.reply({ content: content.content }) : await message.channel.send({ content: content.content });
-          }
-        });
+    const args = message.content.split(" ");
 
-        break;
-      }
-    }
+    const autoReplyItem = this.autoReplyData.find((value) =>
+      value.wildcard ? message.content.includes(value.match) : message.content.startsWith(value.match)
+    );
+
+    if (!autoReplyItem) return;
+
+    autoReplyItem.content.forEach(async (content, index) => {
+      let sendText = "undefined";
+      let mentionText = undefined;
+      const mentionedUser = message.mentions.users.first();
+      if (!mentionedUser && autoReplyItem.mentionUser) return;
+      else mentionText = `<@${mentionedUser.id}> `;
+
+      sendText = index == 0 ? `${mentionText}${content.content}` : content.content;
+      content.replyMessage && message.reference && message.reference.messageId ? await message.reply(sendText) : await message.channel.send(sendText);
+    });
   };
 }
