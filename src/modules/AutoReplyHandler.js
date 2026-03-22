2e9c7660a6b25372ce70377fd153d7fb1d71defd
QuangHuy/JustASweetPotato <kehuydiet09094@gmail.com>
2026-03-22 14:36:18 +0700

    Fix bug

--- src/modules/AutoReplyHandler.js
+++ src/modules/AutoReplyHandler.js
@@ -1,5 +1,61 @@
 import { Message } from "discord.js";
 import Handler from "./Handler.js";
+import fs from "fs";
+import { pipeline } from "stream/promises";
+
+/**
+ *
+ * @param {string} content
+ * @returns
+ */
+function findFBUrl(content) {
+  const urls = content.match(/https?:\/\/[^\s]+/g);
+  if (!urls) return undefined;
+  const fbUrls = urls.filter((url) => url.startsWith("https://www.facebook.com"));
+  return fbUrls.at(0);
+}
+
+/**
+ *
+ * @param {Message<true>} message
+ */
+async function linkConvert(message) {
+  const url = findFBUrl(message.content);
+  if (!url) return;
+  return url.replace("https://www.facebook.com", "http://python:9812");
+}
+
+function extractVideoLink(html) {
+  const match = html.match(/<meta property="og:video:secure_url" content="([^"]+)"/);
+  return match ? match[1] : null;
+}
+
+function extractReelId(html) {
+  const match = html.match(
+    /<meta property="og:url" content="https:\/\/www\.facebook\.com\/reel\/(\d+)"/
+  );
+  return match ? match[1] : null;
+}
+
+async function downloadVideo(url, path = "video.mp4") {
+  const res = await fetch(url);
+
+  if (!res.ok) throw new Error("Download fail");
+
+  const fileStream = fs.createWriteStream(path);
+
+  const body = res.body;
+
+  if (body) {
+    await pipeline(res.body, fileStream);
+  } else {
+    return undefined;
+  }
+
+  return path;
+}
+
+const cache = {};
 
 export default class AutoReplyHandler extends Handler {
   autoReplyData = [
@@ -25,25 +81,39 @@ export default class AutoReplyHandler extends Handler {
    *
    * @param {Message<true>} message
    */
-  async linkConvert(message) {
-    const url = this.findFBUrl(message.content);
-    if (!url) return;
-    const newUrl = url.replace("facebook.com", "facebed.com");
-    await message.channel.send({
-      content: `From ${message.author.username} > ${newUrl}`,
-    });
-  }
+  async processFacebookUrl(message) {
+    try {
+      if (!message.inGuild() || message.author.bot) return;
 
-  /**
-   *
-   * @param {string} content
-   * @returns
-   */
-  findFBUrl(content) {
-    const urls = content.match(/https?:\/\/[^\s]+/g);
-    if (!urls) return undefined;
-    const fbUrls = urls.filter((url) => url.startsWith("https://www.facebook.com"));
-    return fbUrls.at(0);
+      const linkConverted = await linkConvert(message);
+
+      if (!linkConverted) return;
+
+      const text = await (await fetch(linkConverted)).text();
+
+      const videoLink = extractVideoLink(text);
+      const videoReelId = extractReelId(text);
+
+      if (!videoLink) return;
+
+      if (cache[videoReelId]) {
+        await message.reply({ content: cache[videoReelId] });
+        return;
+      }
+
+      const path = await downloadVideo(videoLink, `${videoReelId}.mp4`);
+
+      if (!path) return;
+
+      const sendMessage = await message.reply({ files: [path] });
+
+      cache[videoReelId] = sendMessage.attachments.at(0).proxyURL;
+
+      if (fs.readFileSync(path));
+      fs.unlinkSync(path);
+    } catch (error) {
+      this.client.logger.writeLog(error);
+    }
   }
 
   /**
@@ -52,11 +122,10 @@ export default class AutoReplyHandler extends Handler {
    */
   onMessage = async (message) => {
     try {
+      if (message.author.bot) return;
       if (message.content.startsWith("c>")) return;
 
-      this.linkConvert(message);
-
-      const args = message.content.split(" ");
+      this.processFacebookUrl(message);
 
       const autoReplyItem = this.autoReplyData.find((value) =>
         value.wildcard
