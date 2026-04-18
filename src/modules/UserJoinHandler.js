990916d5675bf4842b84b75e95c7900eea07e45e
QuangHuy/JustASweetPotato <kehuydiet09094@gmail.com>
2026-04-19 01:44:39 +0700

    Change kick clone accout time to 1 day"

--- src/modules/UserJoinHandler.js
+++ src/modules/UserJoinHandler.js
@@ -11,54 +11,46 @@ export default class UserJoinHandler extends Handler {
     super(options);
 
     this.guild = this.client.guilds.cache.get("811939594882777128");
-    this.kickTime = 5 * 60 * 1000;
+    this.kickTime = 1 * 60 * 60 * 1000;
 
-    setInterval(async () => {
-      try {
-        if (!this.guild)
-          this.guild = await this.client.guilds.fetch("811939594882777128");
+    setInterval(this.systemInterval, 5 * 60 * 1000);
+  }
 
-        let memberKickList = [];
+  async systemInterval() {
+    try {
+      if (!this.guild) this.guild = await this.client.guilds.fetch("811939594882777128");
 
-        const rows = await this.client.joinSessionService.getAll();
-        rows.forEach((userData) => {
-          const timestamp = parseInt(userData.join_timestamp ?? "0");
-          if (timestamp == 0) {
-            throw new Error(`Invalid timestamp format, value: ${userData}`);
-          }
+      const rows = await this.client.joinSessionService.getAll();
 
-          const member = this.guild.members.cache.get(userData.id);
+      for (const row of rows) {
+        const timestamp = parseInt(row.join_timestamp ?? "0");
+        if (timestamp == 0) {
+          throw new Error(`Invalid timestamp format, value: ${row}`);
+        }
 
-          if (!member) return;
+        const member = this.guild.members.cache.get(row.id);
 
+        if (!member) return;
+
+        if (timestamp - member.joinedTimestamp > this.kickTime) {
           const role = member.roles.cache.get("1301219249842556938");
 
-          if (!role) {
-            memberKickList.push(member);
-          }
-        });
-
-        for (const member of memberKickList) {
-          try {
-            if (member instanceof GuildMember) {
-              await member.send({
-                content:
-                  "You have been kicked from Thiên Hà Của Sứa with reason: Don't have role ***Neuron*** after join server 5 minutes",
-              });
-            }
+          if (!role && member instanceof GuildMember) {
+            await member.send({
+              content:
+                "You have been kicked from Thiên Hà Của Sứa with reason: Don't have role ***Neuron*** after join server 1 day (anti clone)",
+            });
 
             setTimeout(
               () => member.kick().catch((err) => this.client.logger.writeLog(err)),
               5000
             );
-          } catch (error) {
-            this.client.logger.writeLog(error);
           }
         }
-      } catch (error) {
-        this.client.logger.writeLog(error);
       }
-    }, this.kickTime);
+    } catch (error) {
+      this.client.logger.writeLog(error);
+    }
   }
 
   /**
