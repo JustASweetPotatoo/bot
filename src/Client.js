2d37f90f4bea8aa8f94172848dbb8aed0e666fd1
QuangHuy <kehuydiet09094@gmail.com>
2026-05-19 22:06:09 +0700

    Add catch function to detect error

--- src/Client.js
+++ src/Client.js
@@ -94,12 +94,24 @@ class MossClient extends Client {
       if (message.author.bot) return;
 
       try {
-        this.prefixCommandHandler.onMessage(message);
-        this.levelHandler.onMessage(message);
-        this.autoReplyHandler.onMessage(message);
-        this.noichuHandler.onMessage(message);
-        this.NSFWHandler.onMessage(message);
-        this.pinMessageHandler.onMessage(message);
+        this.prefixCommandHandler.onMessage(message).catch(error => {
+          this.logger.writeLog(error);
+        });
+        this.levelHandler.onMessage(message).catch(error => {
+          this.logger.writeLog(error);
+        });
+        this.autoReplyHandler.onMessage(message).catch(error => {
+          this.logger.writeLog(error);
+        });
+        this.noichuHandler.onMessage(message).catch(error => {
+          this.logger.writeLog(error);
+        });
+        this.NSFWHandler.onMessage(message).catch(error => {
+          this.logger.writeLog(error);
+        });
+        this.pinMessageHandler.onMessage(message).catch(error => {
+          this.logger.writeLog(error);
+        });
       } catch (error) {
         const replyMessage = await message.reply({
           embeds: [
